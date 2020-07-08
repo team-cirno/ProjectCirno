f2228cd0454a55de1589f8ace1176016581fe2f6
AnnXnyZ <48843406+AnnXnyZ@users.noreply.github.com>
2020-07-08 00:13:44 -0400

    logger update

--- src/Logger/Logger.java
+++ src/Logger/Logger.java
@@ -1,4 +1,43 @@
 package Logger;
 
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.sql.Timestamp;
+import java.util.ArrayList;
+
 public class Logger {
+    private static String curClass = null;
+    private static ArrayList<String> logSet = new ArrayList();
+
+    public Logger(Object obj){
+        this.curClass=obj.getClass().getSimpleName();
+    }
+
+    public static void log(String logEntry){
+        String time = new Timestamp(System.currentTimeMillis()).toString();
+        exportLog(time+"\t"+curClass+"\t"+logEntry);
+    }
+
+    public static void exportLog(String log){
+        try{
+            BufferedWriter logWriter = new BufferedWriter(new FileWriter("./logger.txt",true));
+            logWriter.write(log);
+            logWriter.close();
+        }catch(IOException e){
+            e.printStackTrace();
+        }
+    }
+
+    public static void clearLog(){
+        try{
+            BufferedWriter logWriter = new BufferedWriter(new FileWriter("./logger.txt"));
+            logWriter.write("");
+            logWriter.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
 }

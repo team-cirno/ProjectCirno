3ba1b18eac129114337d1e5c7e6a4570a3a83e24
ReL Rail <FantasyReimu@gmail.com>
2020-07-12 02:49:29 +0800

    i

--- src/http/HttpHandler.java
+++ src/http/HttpHandler.java
@@ -25,7 +25,7 @@ public class HttpHandler {
 
 
     public static void main(String... args) {
-        var Address = new InetSocketAddress("10.0.0.16", 8080);
+        var Address = new InetSocketAddress("10.0.0.90", 2048);
 
         startMultiThreaded(Address);
     }
@@ -72,7 +72,7 @@ public class HttpHandler {
     private static ServerSocket getServerSocket(InetSocketAddress address)
             throws Exception {
 
-        return new ServerSocket(8080);
+        return new ServerSocket(2048);
         /*
         // Backlog is the maximum number of pending connections on the socket,
         // 0 means that an implementation-specific default is used
--- src/http/PEMImporter.java
+++ src/http/PEMImporter.java
@@ -1,15 +1,5 @@
 package http;
 
-import org.bouncycastle.cert.X509CertificateHolder;
-import org.bouncycastle.cert.jcajce.JcaX509CertificateConverter;
-import org.bouncycastle.jce.provider.BouncyCastleProvider;
-import org.bouncycastle.openssl.PEMDecryptorProvider;
-import org.bouncycastle.openssl.PEMEncryptedKeyPair;
-import org.bouncycastle.openssl.PEMKeyPair;
-import org.bouncycastle.openssl.PEMParser;
-import org.bouncycastle.openssl.jcajce.JcaPEMKeyConverter;
-import org.bouncycastle.openssl.jcajce.JcePEMDecryptorProviderBuilder;
-
 import javax.net.ssl.KeyManagerFactory;
 import javax.net.ssl.SSLContext;
 import javax.net.ssl.SSLSocketFactory;
@@ -24,94 +14,5 @@ import java.security.cert.X509Certificate;
 
 public class PEMImporter {
 
-
-    public static SSLSocketFactory getSocketFactory(final String caCrtFile, final String crtFile, final String keyFile,
-                                                    final String password) {
-        try {
-
-            /**
-             * Add BouncyCastle as a Security Provider
-             */
-            Security.addProvider(new BouncyCastleProvider());
-
-            JcaX509CertificateConverter certificateConverter = new JcaX509CertificateConverter().setProvider("BC");
-
-            /**
-             * Load Certificate Authority (CA) certificate
-             */
-            PEMParser reader = new PEMParser(new FileReader(caCrtFile));
-            X509CertificateHolder caCertHolder = (X509CertificateHolder) reader.readObject();
-            reader.close();
-
-            X509Certificate caCert = certificateConverter.getCertificate(caCertHolder);
-
-            /**
-             * Load client certificate
-             */
-            reader = new PEMParser(new FileReader(crtFile));
-            X509CertificateHolder certHolder = (X509CertificateHolder) reader.readObject();
-            reader.close();
-
-            X509Certificate cert = certificateConverter.getCertificate(certHolder);
-
-            /**
-             * Load client private key
-             */
-            reader = new PEMParser(new FileReader(keyFile));
-            Object keyObject = reader.readObject();
-            reader.close();
-
-            PEMDecryptorProvider provider = new JcePEMDecryptorProviderBuilder().build(password.toCharArray());
-            JcaPEMKeyConverter keyConverter = new JcaPEMKeyConverter().setProvider("BC");
-
-            KeyPair key;
-
-            if (keyObject instanceof PEMEncryptedKeyPair) {
-                key = keyConverter.getKeyPair(((PEMEncryptedKeyPair) keyObject).decryptKeyPair(provider));
-            } else {
-                key = keyConverter.getKeyPair((PEMKeyPair) keyObject);
-            }
-
-            /**
-             * CA certificate is used to authenticate server
-             */
-            KeyStore caKeyStore = KeyStore.getInstance(KeyStore.getDefaultType());
-            caKeyStore.load(null, null);
-            caKeyStore.setCertificateEntry("ca-certificate", caCert);
-
-            TrustManagerFactory trustManagerFactory = TrustManagerFactory.getInstance(
-                    TrustManagerFactory.getDefaultAlgorithm());
-            trustManagerFactory.init(caKeyStore);
-
-            /**
-             * Client key and certificates are sent to server so it can authenticate the client
-             */
-            KeyStore clientKeyStore = KeyStore.getInstance(KeyStore.getDefaultType());
-            clientKeyStore.load(null, null);
-            clientKeyStore.setCertificateEntry("certificate", cert);
-            clientKeyStore.setKeyEntry("private-key", key.getPrivate(), password.toCharArray(),
-                    new Certificate[]{cert});
-
-            KeyManagerFactory keyManagerFactory = KeyManagerFactory.getInstance(
-                    KeyManagerFactory.getDefaultAlgorithm());
-            keyManagerFactory.init(clientKeyStore, password.toCharArray());
-
-            /**
-             * Create SSL socket factory
-             */
-            SSLContext context = SSLContext.getInstance("TLSv1.2");
-            context.init(keyManagerFactory.getKeyManagers(), trustManagerFactory.getTrustManagers(), null);
-
-            /**
-             * Return the newly created socket factory object
-             */
-            return context.getSocketFactory();
-
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-
-        return null;
-    }
 
 }

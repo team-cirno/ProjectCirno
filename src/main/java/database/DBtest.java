d661e515218bc94d9506b2be2665beb17dbdef42
ReL Rail <FantasyReimu@gmail.com>
2020-08-02 04:21:55 +0800

    mongo db

--- src/main/java/database/DBtest.java
+++ src/main/java/database/DBtest.java
@@ -2,16 +2,24 @@ package database;
 
 import com.mongodb.MongoClient;
 import com.mongodb.MongoClientURI;
+import com.mongodb.MongoCredential;
 import com.mongodb.client.MongoDatabase;
 
 public class DBtest {
     public static void main(String[] args) {
 
+        // Creating a Mongo client
+        MongoClient mongo = new MongoClient( "10.0.0.16" , 27017 );
 
-        MongoClientURI uri = new MongoClientURI(
-                "mongodb+srv://neko:<password>@cluster0.8fyld.mongodb.net/<dbname>?retryWrites=true&w=majority");
+        // Creating Credentials
+        MongoCredential credential;
+        credential = MongoCredential.createCredential("neko", "test",
+                "Nekohub!".toCharArray());
+        System.out.println("Connected to the database successfully");
 
-        MongoClient mongoClient = new MongoClient(uri);
-        MongoDatabase database = mongoClient.getDatabase("test");
+        // Accessing the database
+        MongoDatabase database = mongo.getDatabase("test");
+        System.out.println("Credentials ::"+ credential);
+
     }
 }

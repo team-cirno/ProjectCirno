86116594d4acb9b38acababc93e0f7f3ac220d3d
ReL Rail <FantasyReimu@gmail.com>
2020-08-01 09:58:29 +0800

    u

---
+++ src/database/DBtest.java
@@ -0,0 +1,14 @@
+package database;
+
+public class DBtest {
+    public static void main(String[] args) {
+
+        /*
+            MongoClientURI uri = new MongoClientURI(
+                    "mongodb+srv://neko:<password>@cluster0.8fyld.mongodb.net/<dbname>?retryWrites=true&w=majority");
+
+            MongoClient mongoClient = new MongoClient(uri);
+            MongoDatabase database = mongoClient.getDatabase("test");
+        */
+    }
+}

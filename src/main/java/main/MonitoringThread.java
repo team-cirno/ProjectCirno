e38cd5ec6f11512aff7df69089b7d973bd4a73b6
ReL Rail <FantasyReimu@gmail.com>
2020-08-09 12:52:17 +0800

    Update MonitoringThread.java

--- src/main/java/main/MonitoringThread.java
+++ src/main/java/main/MonitoringThread.java
@@ -14,24 +14,23 @@ public class MonitoringThread extends Thread {
     private long refreshInterval;
     private boolean stopped;
 
-    private Map<Long, ThreadTime> threadTimeMap = new HashMap<Long, ThreadTime>();
     private ThreadMXBean threadBean = ManagementFactory.getThreadMXBean();
     OperatingSystemMXBean mont;
 
     Queue cpuLoad;
     Queue ramLoad;
-    Queue netLoad;
+    Queue thdLoad;
 
     public MonitoringThread(long refreshInterval) {
         this.refreshInterval = refreshInterval;
 
+
         setName("MonitoringThread");
         long[] threadid = new long[1];
         threadid[0] = this.getId();
-        this.mapNewThreads(threadid);
         cpuLoad = new Queue(12);
         ramLoad = new Queue(12);
-        netLoad = new Queue(12);
+        thdLoad = new Queue(12);
         mont = (com.sun.management.OperatingSystemMXBean)
                 ManagementFactory.getOperatingSystemMXBean();
     }
@@ -42,7 +41,7 @@ public class MonitoringThread extends Thread {
 
             cpuLoad.enqueue(mont.getCpuLoad());
             ramLoad.enqueue(mont.getTotalPhysicalMemorySize()-mont.getFreeMemorySize());
-            netLoad.enqueue(0);
+            thdLoad.enqueue(ServerMain.threadCount);
 
             try {
                 Thread.sleep(refreshInterval);
@@ -53,74 +52,8 @@ public class MonitoringThread extends Thread {
         }
     }
 
-    private void mapNewThreads(long[] allThreadIds) {
-        for (long id : allThreadIds) {
-            synchronized (threadTimeMap) {
-                if(!threadTimeMap.containsKey(id))
-                    threadTimeMap.put(id, new ThreadTime(id));
-            }
-        }
-    }
-
-    private void removeDeadThreads(Set<Long> mappedIds, long[] allThreadIds) {
-        outer: for (long id1 : mappedIds) {
-            for (long id2 : allThreadIds) {
-                if(id1 == id2)
-                    continue outer;
-            }
-            synchronized (threadTimeMap) {
-                threadTimeMap.remove(id1);
-            }
-        }
-    }
-
     public void shutdown() {
         this.stopped = true;
     }
 
-    public double getTotalUsage() {
-        Collection<ThreadTime> values;
-        synchronized (threadTimeMap) {
-            values = new HashSet<ThreadTime>(threadTimeMap.values());
-        }
-
-        double usage = 0D;
-        for (ThreadTime threadTime : values) {
-            synchronized (threadTime) {
-                usage += (threadTime.getCurrent() - threadTime.getLast()) / (refreshInterval * 10000);
-            }
-        }
-        return usage;
-    }
-
-    static class ThreadTime {
-
-        private long id;
-        private long last;
-        private long current;
-
-        public ThreadTime(long id) {
-            this.id = id;
-        }
-
-        public long getId() {
-            return id;
-        }
-
-        public long getLast() {
-            return last;
-        }
-
-        public void setLast(long last) {
-            this.last = last;
-        }
-
-        public long getCurrent() {
-            return current;
-        }
-
-        public void setCurrent(long current) {
-            this.current = current;
-        }
-    }
 }
